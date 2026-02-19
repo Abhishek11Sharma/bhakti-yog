18aa77ab59a5f562b988efa83cbabd55be7aafee
Abhishek Sharma <122462157+Abhishek11Sharma@users.noreply.github.com>
2026-02-20 01:15:24 +0530

    Update metadata title and description for ArtiPage

--- src/app/arti/[artiName]/page.tsx
+++ src/app/arti/[artiName]/page.tsx
@@ -2,10 +2,10 @@ import ArtiDetailPage from "@/components/ArtiDetailPage";
 import type { Metadata } from "next";
 
 export function generateMetadata({ params }: { params: { artiName: string } }): Metadata {
-  const title = params.artiName ? `${params.artiName} आरती - Spiritual Path` : `आरती - Spiritual Path`;
+  const title = params.artiName ? `${params.artiName} आरती - Spiritual Marg` : `आरती - Spiritual Marg`;
   return {
     title,
-    description: `Read the ${params.artiName} arti in Hindi on Spiritual Path.`,
+    description: `Read the ${params.artiName} arti in Hindi on Spiritual Marg.`,
   };
 }
 
